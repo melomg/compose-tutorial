02e6c9ef6e47b8969eaee3e40d82c87a991f0867
Melih Gultekin <mmelihgultekin@gmail.com>
2021-02-27 20:31:24 +0100

    Apply round corner shape clip

--- app/src/main/java/com/melih/composetutorial/MainActivity.kt
+++ app/src/main/java/com/melih/composetutorial/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,7 +41,8 @@ fun NewsStory() {
             contentDescription = null,
             modifier = Modifier
                 .height(180.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
             contentScale = ContentScale.Crop,
         )
 
