2bb6b18106e74962cafd7b1f410fa06731756518
Melih Gultekin <mmelihgultekin@gmail.com>
2021-02-27 20:28:12 +0100

    Add Column, Image and Spacer

--- app/src/main/java/com/melih/composetutorial/MainActivity.kt
+++ app/src/main/java/com/melih/composetutorial/MainActivity.kt
@@ -3,11 +3,17 @@ package com.melih.composetutorial
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.melih.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,22 +23,36 @@ class MainActivity : AppCompatActivity() {
             ComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    NewsStory()
                 }
             }
         }
     }
+
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop,
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
+    NewsStory()
 }
