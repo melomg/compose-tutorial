ac4557883e958b5a33572732ae551bb24c8a985a
Melih Gultekin <mmelihgultekin@gmail.com>
2021-02-28 13:45:47 +0100

    Animate list items by adding animateColorAsState to list items for toggle

--- app/src/main/java/com/melih/composetutorial/MainActivity.kt
+++ app/src/main/java/com/melih/composetutorial/MainActivity.kt
@@ -3,15 +3,16 @@ package com.melih.composetutorial
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -144,5 +145,12 @@ fun ClickCounter(clickCount: Int, onClick: () -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if(isSelected) Color.Red else Color.Transparent)
+
+    Text(text = "Hello $name!", modifier = Modifier
+        .padding(24.dp)
+        .background(color = backgroundColor)
+        .clickable { isSelected = !isSelected }
+    )
 }
